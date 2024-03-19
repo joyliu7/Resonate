d73a4d6705957b42d4fa0a1da15c63041735fcf4
Joy Liu <joyliuuu@gmail.com>
2024-03-19 10:13:07 -0700

    update classification code

--- websitedemo/classify.py
+++ websitedemo/classify.py
@@ -1,20 +1,19 @@
 import cohere
 import json
+from cohere.responses.classify import Example
 co = cohere.Client('jY0mYl6MTtHnKLxMAPYBGg69rWrxt2CW9Kh0J1TJ')
-from cohere.classify import Example
 
-examples=[  Example("I want to have lunch", "eat out"),  Example("I want to have apple", "eat out"),  Example("I want to go to cafe", "eat out"),  Example("I want to have try new restaurants", "eat out"),  Example("I want to train", "gym buddy"),  Example("I want to exercise", "gym buddy"),  
+
+examples=[ 
+          Example("I want to have lunch", "eat out"),  Example("I want to have apple", "eat out"),  Example("I want to go to cafe", "eat out"),  Example("I want to have try new restaurants", "eat out"),  Example("I want to train", "gym buddy"),  Example("I want to exercise", "gym buddy"),  
           Example("I want to do cardio", "gym buddy"),Example("I want some to teach me", "Looking for mentor"), Example("I want to learn css", "Looking for mentor"), Example("I want to learn skiing", "Looking for mentor")
-          
           ]
-inputs=["I want to eat apple"]
+inputs=["I want to have lunch", "I want to go to the gym", "I want to find a mentor"]
 
 response = co.classify(
+  model='embed-english-v2.0',
   inputs=inputs,
   examples=examples,
 )
 classifications_dict = vars(response)
-# Write the response to a local JSON file
-with open('response.json', 'w') as f:
-    json.dump(response, f)
-
+print('The confidence levels of the labels are: {}'.format(response.classifications))
